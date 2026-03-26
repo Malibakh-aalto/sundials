Malibakh-aalto/sundials

commit 615d176f5f75b31fa9cf051bad938f197a8b6a3e

Bugfix: Disable logging output with empty string (#860)
Passing `NULL` or an empty string disables logging output (Fixes #844)

---------

Co-authored-by: Steven Roberts <roberts115@llnl.gov>
Co-authored-by: David J. Gardner <gardner48@llnl.gov>

src/sundials/sundials_logger.c
@@ -224,6 +224,14 @@ SUNErrCode SUNLogger_SetErrorFilename(SUNLogger logger, const char* error_filena
 
   if (!sunLoggerIsOutputRank(logger, NULL)) { return SUN_SUCCESS; }
 
+  /* An empty filename disables output for this stream. */
+  if (!error_filename || error_filename[0] == '\0')
+  {
+    /* Don't close the file here, that is managed by the underlying hashmap */
+    logger->error_fp = NULL;
+    return SUN_SUCCESS;
+  }
+
   if (error_filename && strcmp(error_filename, ""))
   {
 #if SUNDIALS_LOGGING_LEVEL >= SUNDIALS_LOGGING_ERROR
@@ -255,6 +263,14 @@ SUNErrCode SUNLogger_SetWarningFilename(SUNLogger logger,
 
   if (!sunLoggerIsOutputRank(logger, NULL)) { return SUN_SUCCESS; }
 
+  /* An empty filename disables output for this stream. */
+  if (!warning_filename || warning_filename[0] == '\0')
+  {
+    /* Don't close the file here, that is managed by the underlying hashmap */
+    logger->warning_fp = NULL;
+    return SUN_SUCCESS;
+  }
+
   if (warning_filename && strcmp(warning_filename, ""))
   {
 #if SUNDIALS_LOGGING_LEVEL >= SUNDIALS_LOGGING_WARNING
@@ -285,6 +301,14 @@ SUNErrCode SUNLogger_SetInfoFilename(SUNLogger logger, const char* info_filename
 
   if (!sunLoggerIsOutputRank(logger, NULL)) { return SUN_SUCCESS; }
 
+  /* An empty filename disables output for this stream. */
+  if (!info_filename || info_filename[0] == '\0')
+  {
+    /* Don't close the file here, that is managed by the underlying hashmap */
+    logger->info_fp = NULL;
+    return SUN_SUCCESS;
+  }
+
   if (info_filename && strcmp(info_filename, ""))
   {
 #if SUNDIALS_LOGGING_LEVEL >= SUNDIALS_LOGGING_INFO
@@ -314,6 +338,14 @@ SUNErrCode SUNLogger_SetDebugFilename(SUNLogger logger, const char* debug_filena
   if (!logger) { return SUN_ERR_ARG_CORRUPT; }
 
   if (!sunLoggerIsOutputRank(logger, NULL)) { return SUN_SUCCESS; }
+
+  /* An empty filename disables output for this stream. */
+  if (!debug_filename || debug_filename[0] == '\0')
+  {
+    /* Don't close the file here, that is managed by the underlying hashmap */
+    logger->debug_fp = NULL;
+    return SUN_SUCCESS;
+  }
 
   if (debug_filename && strcmp(debug_filename, ""))
   {

test/unit_tests/sundials/test_sundials_logger.cpp
@@ -1,0 +1,170 @@
+/* -----------------------------------------------------------------
+ * SUNDIALS Copyright Start
+ * Copyright (c) 2025-2026, Lawrence Livermore National Security,
+ * University of Maryland Baltimore County, and the SUNDIALS contributors.
+ * Copyright (c) 2013-2025, Lawrence Livermore National Security
+ * and Southern Methodist University.
+ * Copyright (c) 2002-2013, Lawrence Livermore National Security.
+ * All rights reserved.
+ *
+ * See the top-level LICENSE and NOTICE files for details.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SUNDIALS Copyright End
+ * -----------------------------------------------------------------*/
+
+#include <algorithm>
+#include <cstdio>
+#include <fstream>
+#include <gtest/gtest.h>
+#include <string>
+
+#include <sundials/priv/sundials_logger_macros.h>
+#include <sundials/sundials_errors.h>
+#include <sundials/sundials_logger.h>
+
+// Add [[maybe_unused]] because ReadFile and CountLines are "unused" when the
+// logging level is set to 0
+
+[[maybe_unused]] static std::string ReadFile(const std::string& path)
+{
+  std::ifstream file(path);
+  return {std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+}
+
+[[maybe_unused]] static int CountLines(const std::string& s)
+{
+  return std::count(s.begin(), s.end(), '\n');
+}
+
+TEST(SUNLoggerTest, EmptyFilenameDisablesErrorOutput)
+{
+#if SUNDIALS_LOGGING_LEVEL < SUNDIALS_LOGGING_ERROR
+  GTEST_SKIP() << "Errors not enabled in this build";
+#else
+  const std::string errfile = "test_sundials_logger.err";
+
+  (void)std::remove(errfile.c_str());
+
+  SUNLogger logger = NULL;
+  ASSERT_EQ(SUNLogger_Create(SUN_COMM_NULL, 0, &logger), SUN_SUCCESS);
+
+  ASSERT_EQ(SUNLogger_SetErrorFilename(logger, errfile.c_str()), SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_QueueMsg(logger, SUN_LOGLEVEL_ERROR, "scope", "label",
+                               "first"),
+            SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_Flush(logger, SUN_LOGLEVEL_ERROR), SUN_SUCCESS);
+
+  EXPECT_EQ(CountLines(ReadFile(errfile)), 1);
+
+  ASSERT_EQ(SUNLogger_SetErrorFilename(logger, ""), SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_QueueMsg(logger, SUN_LOGLEVEL_ERROR, "scope", "label",
+                               "second"),
+            SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_Flush(logger, SUN_LOGLEVEL_ERROR), SUN_SUCCESS);
+
+  EXPECT_EQ(CountLines(ReadFile(errfile)), 1);
+
+  ASSERT_EQ(SUNLogger_Destroy(&logger), SUN_SUCCESS);
+  (void)std::remove(errfile.c_str());
+#endif
+}
+
+TEST(SUNLoggerTest, EmptyFilenameDisablesWarningOutput)
+{
+#if SUNDIALS_LOGGING_LEVEL < SUNDIALS_LOGGING_WARNING
+  GTEST_SKIP() << "Warnings not enabled in this build";
+#else
+  const std::string warnfile = "test_sundials_logger.warn";
+
+  (void)std::remove(warnfile.c_str());
+
+  SUNLogger logger = NULL;
+  ASSERT_EQ(SUNLogger_Create(SUN_COMM_NULL, 0, &logger), SUN_SUCCESS);
+
+  ASSERT_EQ(SUNLogger_SetWarningFilename(logger, warnfile.c_str()), SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_QueueMsg(logger, SUN_LOGLEVEL_WARNING, "scope", "label",
+                               "first"),
+            SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_Flush(logger, SUN_LOGLEVEL_WARNING), SUN_SUCCESS);
+
+  EXPECT_EQ(CountLines(ReadFile(warnfile)), 1);
+
+  ASSERT_EQ(SUNLogger_SetWarningFilename(logger, ""), SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_QueueMsg(logger, SUN_LOGLEVEL_WARNING, "scope", "label",
+                               "second"),
+            SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_Flush(logger, SUN_LOGLEVEL_WARNING), SUN_SUCCESS);
+
+  EXPECT_EQ(CountLines(ReadFile(warnfile)), 1);
+
+  ASSERT_EQ(SUNLogger_Destroy(&logger), SUN_SUCCESS);
+  (void)std::remove(warnfile.c_str());
+#endif
+}
+
+TEST(SUNLoggerTest, EmptyFilenameDisablesInfoOutput)
+{
+#if SUNDIALS_LOGGING_LEVEL < SUNDIALS_LOGGING_INFO
+  GTEST_SKIP() << "Info logging not enabled in this build";
+#else
+  const std::string infofile = "test_sundials_logger.info";
+
+  (void)std::remove(infofile.c_str());
+
+  SUNLogger logger = NULL;
+  ASSERT_EQ(SUNLogger_Create(SUN_COMM_NULL, 0, &logger), SUN_SUCCESS);
+
+  ASSERT_EQ(SUNLogger_SetInfoFilename(logger, infofile.c_str()), SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_QueueMsg(logger, SUN_LOGLEVEL_INFO, "scope", "label",
+                               "first"),
+            SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_Flush(logger, SUN_LOGLEVEL_INFO), SUN_SUCCESS);
+
+  EXPECT_EQ(CountLines(ReadFile(infofile)), 1);
+
+  ASSERT_EQ(SUNLogger_SetInfoFilename(logger, ""), SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_QueueMsg(logger, SUN_LOGLEVEL_INFO, "scope", "label",
+                               "second"),
+            SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_Flush(logger, SUN_LOGLEVEL_INFO), SUN_SUCCESS);
+
+  EXPECT_EQ(CountLines(ReadFile(infofile)), 1);
+
+  ASSERT_EQ(SUNLogger_Destroy(&logger), SUN_SUCCESS);
+  (void)std::remove(infofile.c_str());
+#endif
+}
+
+TEST(SUNLoggerTest, EmptyFilenameDisablesDebugOutput)
+{
+#if SUNDIALS_LOGGING_LEVEL < SUNDIALS_LOGGING_DEBUG
+  GTEST_SKIP() << "Debug logging not enabled in this build";
+#else
+  const std::string debugfile = "test_sundials_logger.debug";
+
+  (void)std::remove(debugfile.c_str());
+
+  SUNLogger logger = NULL;
+  ASSERT_EQ(SUNLogger_Create(SUN_COMM_NULL, 0, &logger), SUN_SUCCESS);
+
+  ASSERT_EQ(SUNLogger_SetDebugFilename(logger, debugfile.c_str()), SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_QueueMsg(logger, SUN_LOGLEVEL_DEBUG, "scope", "label",
+                               "first"),
+            SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_Flush(logger, SUN_LOGLEVEL_DEBUG), SUN_SUCCESS);
+
+  EXPECT_EQ(CountLines(ReadFile(debugfile)), 1);
+
+  ASSERT_EQ(SUNLogger_SetDebugFilename(logger, ""), SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_QueueMsg(logger, SUN_LOGLEVEL_DEBUG, "scope", "label",
+                               "second"),
+            SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_Flush(logger, SUN_LOGLEVEL_DEBUG), SUN_SUCCESS);
+
+  EXPECT_EQ(CountLines(ReadFile(debugfile)), 1);
+
+  ASSERT_EQ(SUNLogger_Destroy(&logger), SUN_SUCCESS);
+  (void)std::remove(debugfile.c_str());
+#endif
+}

.github/workflows/ubuntu-latest-clang.yml
@@ -46,7 +46,7 @@ jobs:
         -D ENABLE_WARNINGS_AS_ERRORS=ON \
         -D SUNDIALS_TEST_ENABLE_DEV_TESTS=ON \
         -D SUNDIALS_TEST_ENABLE_UNIT_TESTS=ON \
-        -D SUNDIALS_TEST_ENABLE_GTEST=OFF \
+        -D SUNDIALS_TEST_ENABLE_GTEST=ON \
         -D SUNDIALS_TEST_FLOAT_PRECISION=4 \
         -D SUNDIALS_TEST_INTEGER_PRECISION=10
 

cmake/SundialsSetupCXX.cmake
@@ -33,7 +33,8 @@ sundials_option(CMAKE_CXX_STANDARD_REQUIRED BOOL "Require C++ standard version"
 
 if(SUNDIALS_ENABLE_PYTHON
    OR SUNDIALS_ENABLE_SYCL
-   OR SUNDIALS_ENABLE_GINKGO)
+   OR SUNDIALS_ENABLE_GINKGO
+   OR SUNDIALS_TEST_ENABLE_GTEST)
   set(DOCSTR "The C++ standard to use if C++ is enabled (17, 20, 23)")
   sundials_option(CMAKE_CXX_STANDARD STRING "${DOCSTR}" "17" OPTIONS "17;20;23")
 else()

test/unit_tests/sundials/CMakeLists.txt
@@ -15,7 +15,7 @@
 
 # List of test tuples of the form "name\;args"
 set(unit_tests "test_sundials_datanode\;" "test_sundials_stlvector\;"
-               "test_sundials_hashmap\;")
+               "test_sundials_hashmap\;" "test_sundials_logger\;")
 
 if(SUNDIALS_ENABLE_ERROR_CHECKS)
   list(APPEND unit_tests "test_sundials_errors\;")
@@ -34,6 +34,10 @@ if(TARGET GTest::gtest_main AND TARGET GTest::gmock)
       ${test} PRIVATE sundials_core sundials_nvecserial sundials_sunmemsys_obj
                       GTest::gtest_main GTest::gmock)
 
-    gtest_discover_tests(${test})
+    if(test STREQUAL "test_sundials_logger")
+      gtest_discover_tests(${test} PROPERTIES LABELS "logging")
+    else()
+      gtest_discover_tests(${test})
+    endif()
   endforeach()
 endif()

commit aec807622f8e15fcfaf23bd71f1b1d27023ef2e8

Bugfix: Default logger not printing errors to stderr and warnings to stdout (#882)
Resolves a bug introduced in #860 where the default logger had it's
error and warning file pointers overwritten to NULL.

---------

Co-authored-by: David Gardner <gardner48@llnl.gov>

src/sundials/sundials_logger.c
@@ -38,9 +38,6 @@
 #include "sundials_macros.h"
 #include "sundials_utils.h"
 
-/* default number of files that we allocate space for */
-#define SUN_DEFAULT_LOGFILE_HANDLES_ 8
-
 void sunCreateLogMessage(SUNLogLevel lvl, int rank, const char* scope,
                          const char* label, const char* txt, va_list args,
                          char** log_msg)
@@ -77,6 +74,10 @@ void sunCreateLogMessage(SUNLogLevel lvl, int rank, const char* scope,
 }
 
 #if SUNDIALS_LOGGING_LEVEL > 0
+
+/* default number of files that we allocate space for */
+#define SUN_DEFAULT_LOGFILE_HANDLES_ 8
+
 static FILE* sunOpenLogFile(const char* fname, const char* mode)
 {
   FILE* fp = NULL;
@@ -90,7 +91,6 @@ static FILE* sunOpenLogFile(const char* fname, const char* mode)
 
   return fp;
 }
-#endif
 
 static void sunCloseLogFile(void* fp)
 {
@@ -129,6 +129,33 @@ static sunbooleantype sunLoggerIsOutputRank(SUNDIALS_MAYBE_UNUSED SUNLogger logg
   return retval;
 }
 
+static SUNErrCode sunLoggerSetFilename(SUNLogger logger, const char* filename,
+                                       FILE** fp)
+{
+  if (!sunLoggerIsOutputRank(logger, NULL)) { return SUN_SUCCESS; }
+
+  /* An empty or NULL filename disables output for this stream. */
+  if (sunIsNullOrEmpty(filename))
+  {
+    /* Don't close the file here, that is managed by the underlying hashmap */
+    *fp = NULL;
+    return SUN_SUCCESS;
+  }
+
+  int64_t err = SUNHashMap_GetValue(logger->filenames, filename, (void**)fp);
+  if (err == SUNHASHMAP_ERROR) { return SUN_ERR_FILE_OPEN; }
+  else if (err == SUNHASHMAP_KEYNOTFOUND)
+  {
+    *fp = sunOpenLogFile(filename, "w+");
+    if (*fp == NULL) { return SUN_ERR_FILE_OPEN; }
+
+    err = SUNHashMap_Insert(logger->filenames, filename, (void*)*fp);
+    if (err != 0) { return SUN_ERR_FILE_OPEN; }
+  }
+
+  return SUN_SUCCESS;
+}
+
 static SUNErrCode sunLoggerFreeKeyValue(SUNHashMapKeyValue* kv_ptr)
 {
   if (!kv_ptr || !(*kv_ptr)) { return SUN_SUCCESS; }
@@ -138,11 +165,12 @@ static SUNErrCode sunLoggerFreeKeyValue(SUNHashMapKeyValue* kv_ptr)
   return SUN_SUCCESS;
 }
 
+#endif
+
 SUNErrCode SUNLogger_Create(SUNComm comm, int output_rank, SUNLogger* logger_ptr)
 {
-  SUNLogger logger = NULL;
-
-  *logger_ptr = logger = (SUNLogger)malloc(sizeof(struct SUNLogger_));
+  *logger_ptr      = NULL;
+  SUNLogger logger = (SUNLogger)malloc(sizeof(struct SUNLogger_));
   if (logger == NULL) { return SUN_ERR_MALLOC_FAIL; }
 
   /* Attach the comm, duplicating it if MPI is used. */
@@ -171,6 +199,7 @@ SUNErrCode SUNLogger_Create(SUNComm comm, int output_rank, SUNLogger* logger_ptr
   logger->warning_fp = stdout;
   logger->debug_fp   = NULL;
   logger->info_fp    = NULL;
+#if SUNDIALS_LOGGING_LEVEL > 0
   if (sunLoggerIsOutputRank(logger, NULL))
   {
     /* We store the FILE* in a hash map so that we can ensure
@@ -179,7 +208,9 @@ SUNErrCode SUNLogger_Create(SUNComm comm, int output_rank, SUNLogger* logger_ptr
     SUNHashMap_New(SUN_DEFAULT_LOGFILE_HANDLES_, sunLoggerFreeKeyValue,
                    &logger->filenames);
   }
+#endif
 
+  *logger_ptr = logger;
   return SUN_SUCCESS;
 }
 
@@ -195,20 +226,33 @@ SUNErrCode SUNLogger_CreateFromEnv(SUNComm comm, SUNLogger* logger_out)
   const char* info_fname_env    = getenv("SUNLOGGER_INFO_FILENAME");
   const char* debug_fname_env   = getenv("SUNLOGGER_DEBUG_FILENAME");
 
-  if (SUNLogger_Create(comm, output_rank, &logger))
-  {
-    err = SUN_ERR_CORRUPT;
-    return err;
-  }
+  if (SUNLogger_Create(comm, output_rank, &logger)) { return SUN_ERR_CORRUPT; }
 
   do {
-    err = SUNLogger_SetErrorFilename(logger, error_fname_env);
-    if (err) { break; }
-    err = SUNLogger_SetWarningFilename(logger, warning_fname_env);
-    if (err) { break; }
-    err = SUNLogger_SetDebugFilename(logger, debug_fname_env);
-    if (err) { break; }
-    err = SUNLogger_SetInfoFilename(logger, info_fname_env);
+    /* Only override the default logging if the env var is defined */
+    if (error_fname_env != NULL)
+    {
+      err = SUNLogger_SetErrorFilename(logger, error_fname_env);
+      if (err) { break; }
+    }
+
+    if (warning_fname_env != NULL)
+    {
+      err = SUNLogger_SetWarningFilename(logger, warning_fname_env);
+      if (err) { break; }
+    }
+
+    if (debug_fname_env != NULL)
+    {
+      err = SUNLogger_SetDebugFilename(logger, debug_fname_env);
+      if (err) { break; }
+    }
+
+    if (info_fname_env != NULL)
+    {
+      err = SUNLogger_SetInfoFilename(logger, info_fname_env);
+      if (err) { break; }
+    }
   }
   while (0);
 
@@ -222,38 +266,12 @@ SUNErrCode SUNLogger_SetErrorFilename(SUNLogger logger, const char* error_filena
 {
   if (!logger) { return SUN_ERR_ARG_CORRUPT; }
 
-  if (!sunLoggerIsOutputRank(logger, NULL)) { return SUN_SUCCESS; }
-
-  /* An empty filename disables output for this stream. */
-  if (!error_filename || error_filename[0] == '\0')
-  {
-    /* Don't close the file here, that is managed by the underlying hashmap */
-    logger->error_fp = NULL;
-    return SUN_SUCCESS;
-  }
-
-  if (error_filename && strcmp(error_filename, ""))
-  {
 #if SUNDIALS_LOGGING_LEVEL >= SUNDIALS_LOGGING_ERROR
-    FILE* fp = NULL;
-    if (!SUNHashMap_GetValue(logger->filenames, error_filename, (void*)&fp))
-    {
-      logger->error_fp = fp;
-    }
-    else
-    {
-      logger->error_fp = sunOpenLogFile(error_filename, "w+");
-      if (logger->error_fp)
-      {
-        SUNHashMap_Insert(logger->filenames, error_filename,
-                          (void*)logger->error_fp);
-      }
-      else { return SUN_ERR_FILE_OPEN; }
-    }
-#endif
-  }
-
+  return sunLoggerSetFilename(logger, error_filename, &logger->error_fp);
+#else
+  ((void)error_filename);
   return SUN_SUCCESS;
+#endif
 }
 
 SUNErrCode SUNLogger_SetWarningFilename(SUNLogger logger,
@@ -261,114 +279,36 @@ SUNErrCode SUNLogger_SetWarningFilename(SUNLogger logger,
 {
   if (!logger) { return SUN_ERR_ARG_CORRUPT; }
 
-  if (!sunLoggerIsOutputRank(logger, NULL)) { return SUN_SUCCESS; }
-
-  /* An empty filename disables output for this stream. */
-  if (!warning_filename || warning_filename[0] == '\0')
-  {
-    /* Don't close the file here, that is managed by the underlying hashmap */
-    logger->warning_fp = NULL;
-    return SUN_SUCCESS;
-  }
-
-  if (warning_filename && strcmp(warning_filename, ""))
-  {
 #if SUNDIALS_LOGGING_LEVEL >= SUNDIALS_LOGGING_WARNING
-    FILE* fp = NULL;
-    if (!SUNHashMap_GetValue(logger->filenames, warning_filename, (void*)&fp))
-    {
-      logger->warning_fp = fp;
-    }
-    else
-    {
-      logger->warning_fp = sunOpenLogFile(warning_filename, "w+");
-      if (logger->warning_fp)
-      {
-        SUNHashMap_Insert(logger->filenames, warning_filename,
-                          (void*)logger->warning_fp);
-      }
-      else { return SUN_ERR_FILE_OPEN; }
-    }
-#endif
-  }
-
+  return sunLoggerSetFilename(logger, warning_filename, &logger->warning_fp);
+#else
+  ((void)warning_filename);
   return SUN_SUCCESS;
+#endif
 }
 
 SUNErrCode SUNLogger_SetInfoFilename(SUNLogger logger, const char* info_filename)
 {
   if (!logger) { return SUN_ERR_ARG_CORRUPT; }
 
-  if (!sunLoggerIsOutputRank(logger, NULL)) { return SUN_SUCCESS; }
-
-  /* An empty filename disables output for this stream. */
-  if (!info_filename || info_filename[0] == '\0')
-  {
-    /* Don't close the file here, that is managed by the underlying hashmap */
-    logger->info_fp = NULL;
-    return SUN_SUCCESS;
-  }
-
-  if (info_filename && strcmp(info_filename, ""))
-  {
 #if SUNDIALS_LOGGING_LEVEL >= SUNDIALS_LOGGING_INFO
-    FILE* fp = NULL;
-    if (!SUNHashMap_GetValue(logger->filenames, info_filename, (void*)&fp))
-    {
-      logger->info_fp = fp;
-    }
-    else
-    {
-      logger->info_fp = sunOpenLogFile(info_filename, "w+");
-      if (logger->info_fp)
-      {
-        SUNHashMap_Insert(logger->filenames, info_filename,
-                          (void*)logger->info_fp);
-      }
-      else { return SUN_ERR_FILE_OPEN; }
-    }
-#endif
-  }
-
+  return sunLoggerSetFilename(logger, info_filename, &logger->info_fp);
+#else
+  ((void)info_filename);
   return SUN_SUCCESS;
+#endif
 }
 
 SUNErrCode SUNLogger_SetDebugFilename(SUNLogger logger, const char* debug_filename)
 {
   if (!logger) { return SUN_ERR_ARG_CORRUPT; }
 
-  if (!sunLoggerIsOutputRank(logger, NULL)) { return SUN_SUCCESS; }
-
-  /* An empty filename disables output for this stream. */
-  if (!debug_filename || debug_filename[0] == '\0')
-  {
-    /* Don't close the file here, that is managed by the underlying hashmap */
-    logger->debug_fp = NULL;
-    return SUN_SUCCESS;
-  }
-
-  if (debug_filename && strcmp(debug_filename, ""))
-  {
 #if SUNDIALS_LOGGING_LEVEL >= SUNDIALS_LOGGING_DEBUG
-    FILE* fp = NULL;
-    if (!SUNHashMap_GetValue(logger->filenames, debug_filename, (void*)&fp))
-    {
-      logger->debug_fp = fp;
-    }
-    else
-    {
-      logger->debug_fp = sunOpenLogFile(debug_filename, "w+");
-      if (logger->debug_fp)
-      {
-        SUNHashMap_Insert(logger->filenames, debug_filename,
-                          (void*)logger->debug_fp);
-      }
-      else { return SUN_ERR_FILE_OPEN; }
-    }
-#endif
-  }
-
+  return sunLoggerSetFilename(logger, debug_filename, &logger->debug_fp);
+#else
+  ((void)debug_filename);
   return SUN_SUCCESS;
+#endif
 }
 
 SUNErrCode SUNLogger_QueueMsg(SUNLogger logger, SUNLogLevel lvl,
@@ -498,29 +438,29 @@ SUNErrCode SUNLogger_GetOutputRank(SUNLogger logger, int* output_rank)
 
 SUNErrCode SUNLogger_Destroy(SUNLogger* logger_ptr)
 {
-  int retval       = 0;
-  SUNLogger logger = NULL;
+  SUNErrCode retval = SUN_SUCCESS;
 
   if (!logger_ptr) { return SUN_SUCCESS; }
 
-  logger = *logger_ptr;
-
+  SUNLogger logger = *logger_ptr;
   if (logger && logger->destroy) { retval = logger->destroy(logger_ptr); }
   else if (logger)
   {
     /* Default implementation */
 
+#if SUNDIALS_LOGGING_LEVEL > 0
     if (sunLoggerIsOutputRank(logger, NULL))
     {
       SUNHashMap_Destroy(&logger->filenames);
     }
+#endif
 
 #if SUNDIALS_MPI_ENABLED
     if (logger->comm != SUN_COMM_NULL) { MPI_Comm_free(&logger->comm); }
 #endif
 
     free(logger);
-    logger = NULL;
+    *logger_ptr = NULL;
   }
 
   return retval;

src/sundials/sundials_utils.h
@@ -30,6 +30,11 @@
 /* width of name field in sunfprintf_<type> for aligning table output */
 #define SUN_TABLE_WIDTH 29
 
+static inline sunbooleantype sunIsNullOrEmpty(const char* str)
+{
+  return str == NULL || str[0] == '\0';
+}
+
 static inline char* sunSignedToString(int64_t val)
 {
   char* str     = NULL;

test/unit_tests/sundials/test_sundials_logger.cpp
@@ -37,6 +37,48 @@
   return std::count(s.begin(), s.end(), '\n');
 }
 
+TEST(SUNLoggerTest, DefaultErrorLogger)
+{
+#if SUNDIALS_LOGGING_LEVEL < SUNDIALS_LOGGING_ERROR
+  GTEST_SKIP() << "Errors not enabled in this build";
+#else
+  testing::internal::CaptureStderr();
+
+  SUNLogger logger = NULL;
+  ASSERT_EQ(SUNLogger_Create(SUN_COMM_NULL, 0, &logger), SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_QueueMsg(logger, SUN_LOGLEVEL_ERROR, "scope", "label",
+                               "test"),
+            SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_Flush(logger, SUN_LOGLEVEL_ERROR), SUN_SUCCESS);
+
+  EXPECT_EQ(CountLines(testing::internal::GetCapturedStderr()), 1);
+
+  ASSERT_EQ(SUNLogger_Destroy(&logger), SUN_SUCCESS);
+  ASSERT_EQ(logger, nullptr);
+#endif
+}
+
+TEST(SUNLoggerTest, DefaultWarningLogger)
+{
+#if SUNDIALS_LOGGING_LEVEL < SUNDIALS_LOGGING_WARNING
+  GTEST_SKIP() << "Warnings not enabled in this build";
+#else
+  testing::internal::CaptureStdout();
+
+  SUNLogger logger = NULL;
+  ASSERT_EQ(SUNLogger_Create(SUN_COMM_NULL, 0, &logger), SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_QueueMsg(logger, SUN_LOGLEVEL_WARNING, "scope", "label",
+                               "test"),
+            SUN_SUCCESS);
+  ASSERT_EQ(SUNLogger_Flush(logger, SUN_LOGLEVEL_ERROR), SUN_SUCCESS);
+
+  EXPECT_EQ(CountLines(testing::internal::GetCapturedStdout()), 1);
+
+  ASSERT_EQ(SUNLogger_Destroy(&logger), SUN_SUCCESS);
+  ASSERT_EQ(logger, nullptr);
+#endif
+}
+
 TEST(SUNLoggerTest, EmptyFilenameDisablesErrorOutput)
 {
 #if SUNDIALS_LOGGING_LEVEL < SUNDIALS_LOGGING_ERROR